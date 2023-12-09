58ab946065d6df1ddb9654d8f4ff0a840e8e8e38
MasaGt <gachapin2236tx@gmail.com>
2023-12-09 17:19:49 +1300

    Refactor test code
    - create prepare method that iniytialize doublylinkedlist instances.

--- test/list/DoublyLikedListTest.java
+++ test/list/DoublyLikedListTest.java
@@ -2,21 +2,31 @@ package list;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class DoublyLikedListTest {
 
+	private DoublyLikedList<String> emptyStrList;
+	private DoublyLikedList<Integer> nonEmptyIntList;
+	
+	@BeforeEach
+	public void prep() {
+		emptyStrList = new DoublyLikedList<String>();
+		nonEmptyIntList = new DoublyLikedList<Integer>(new Integer[] {1,5,10});
+	}
+	
 	@Nested
 	class InitTests {
+		
+		
 		@Test
 		void initEmptyList() {
-			DoublyLikedList<String> emptyStrList = new DoublyLikedList<String>();
 			assertEquals("", emptyStrList.toString());
 		}
 		@Test
 		void initNonEmptyList() {
-			DoublyLikedList<Integer> nonEmptyIntList = new DoublyLikedList<Integer>(new Integer[] {1,5,10});
 			assertEquals("1, 5, 10", nonEmptyIntList.toString());
 		}
 	}
@@ -25,7 +35,6 @@ public class DoublyLikedListTest {
 	class AddTests {
 		@Test
 		void addToNonEmptyList() {
-			DoublyLikedList<Integer> nonEmptyIntList = new DoublyLikedList<Integer>(new Integer[] {1,5,10});
 			nonEmptyIntList.add(200);
 			assertEquals("1, 5, 10, 200", nonEmptyIntList.toString());
 		}
