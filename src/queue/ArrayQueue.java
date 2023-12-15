465e813af7e85744d0b0f508c71667ee35b602fe
MasaGt <gachapin2236tx@gmail.com>
2023-12-15 20:05:26 +1300

    ArrayQueue: refactor, create inner Iterator class and each method uses it if needed

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -1,10 +1,15 @@
 package queue;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class ArrayQueue<T> implements Queue<T> {
 
 	private T[] items;
-	private int size;
 	private final int INIT_SIZE = 10;
+	private int size;
+	private int headIndex;
+	private int tailIndex;
 	
 	/**
 	 * initialize an empty queue
@@ -13,6 +18,8 @@ public class ArrayQueue<T> implements Queue<T> {
 	public ArrayQueue() {
 		items = (T[])new Object[INIT_SIZE];
 		size = 0;
+		headIndex = 0;
+		tailIndex = 0;
 	}
 	/**
 	 * initialize an queue based on the arguments
@@ -30,8 +37,10 @@ public class ArrayQueue<T> implements Queue<T> {
 		if (size >= items.length) {
 			expandCapacity();
 		}
-		items[size] = item;
+		int insertIndex = (headIndex + size) % items.length; 
+		items[insertIndex] = item;
 		size++;
+		tailIndex++;
 	}
 	
 	/**
@@ -41,18 +50,79 @@ public class ArrayQueue<T> implements Queue<T> {
 	private void expandCapacity() {
 		@SuppressWarnings("unchecked")
 		T[] newArray =(T[]) new Object[items.length * 2];
-		for (int i = 0; i < items.length; i++) {
-			newArray[i] = items[i];
+		Iterator<T> itr = iterator();
+		int cursor = 0;
+		while (itr.hasNext()) {
+			newArray[cursor++] = itr.next();
 		}
+		headIndex = 0;
+		tailIndex = cursor;
+		size = tailIndex;
 		items = newArray;
+	}
+	
+	/**
+	 * return the size of this queue
+	 * @return
+	 */
+	public int size() {
+		return size;
 	}
 	
 	@Override
 	public String toString() {
 		String contents = "";
 		for (int i = 0; i < size; i++) {
+			// put , between items
 			contents += i != (size - 1) ? items[i] + ", " : items[i];
 		}
 		return contents;
+	}
+	
+	/**
+	 * return an iterator instance
+	 * @return
+	 */
+	public Iterator<T> iterator() {
+		return new Itr();
+	}
+	
+	/**
+	 * the inner Iterator class to iterate over the queue
+	 */
+	private class Itr implements Iterator<T> {
+		
+		// current pointer to the item of items
+		private int cursor;
+		// the number of items remains
+		private int remaining;
+
+		public Itr() {
+			cursor = headIndex;
+			remaining = size();
+		}
+		
+		/**
+		 * 
+		 * @return true if there is a next item
+		 */
+		@Override
+		public boolean hasNext() {
+			return remaining > 0;
+		}
+
+		/**
+		 * 
+		 * @return next item of this queue
+		 */
+		@Override
+		public T next() {
+			if (remaining <= 0) {
+				throw new NoSuchElementException();
+			}
+			remaining--;
+			cursor = (cursor)%items.length;
+			return items[cursor++];
+		}
 	}
 }
