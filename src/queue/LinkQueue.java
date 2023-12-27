a4da6585d9db3c0f6a0ddc1e683d26a8b8ff65e4
MasaGt <gachapin2236tx@gmail.com>
2023-12-27 21:08:52 +1300

    LinkQueue and LinkQueueTest: create files

---
+++ src/queue/LinkQueue.java
@@ -0,0 +1,55 @@
+package queue;
+
+import java.util.Iterator;
+
+public class LinkQueue<T> implements Queue<T> {
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(T target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ test/queue/LinkQueueTest.java
@@ -0,0 +1,14 @@
+package queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LinkQueueTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
