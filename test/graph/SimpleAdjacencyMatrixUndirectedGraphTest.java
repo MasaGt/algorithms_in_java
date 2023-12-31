6416bd06e7de3087e5c24b4e6ae8ff38cfa0dc15
MasaGt <gachapin2236tx@gmail.com>
2023-12-31 15:32:53 +1300

    SimpleAdjacencyMatrixUndirectedGraphTest: add one more test case
    - normal opertion to SimpleAdjacencyMatrixUndirectedGraph.

--- test/graph/SimpleAdjacencyMatrixUndirectedGraphTest.java
+++ test/graph/SimpleAdjacencyMatrixUndirectedGraphTest.java
@@ -73,4 +73,25 @@ class SimpleAdjacencyMatrixUndirectedGraphTest {
 			assertEquals("", graph.toString());
 		}
 	}
+	
+	@Nested
+	class Operations {
+		@Test
+		void manipulateGraph() {
+			SimpleAdjacencyMatrixUndirectedGraph smallGraph = new SimpleAdjacencyMatrixUndirectedGraph(3);
+			smallGraph.addEdge(0, 0);
+			smallGraph.addEdge(1, 2);
+			assertEquals("[0, 0], [1, 2] [2, 1]", smallGraph.toString());
+			
+			smallGraph.addEdge(0, 2);
+			smallGraph.removeEdge(1, 2);
+			assertEquals("[0, 0], [0, 2] [2, 0]", smallGraph.toString());
+			
+			assertTrue(smallGraph.hasEdge(2, 0));
+			assertFalse(smallGraph.hasEdge(2, 2));
+			
+			smallGraph.clear();
+			assertEquals("", smallGraph.toString());
+		}
+	}
 }
